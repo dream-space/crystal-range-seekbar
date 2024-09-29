41939d3462fea7ae9cb3ef7f15d2e1fb7f09cc1a
Muslim Sidiq <muslimsidiq2@gmail.com>
2024-09-29 14:36:01 +0700

    Bugfix and library

---
+++ app/src/main/java/com/example/crystalrangeseekbar/ui/activities/RangeSlider.java
@@ -0,0 +1,19 @@
+package com.example.crystalrangeseekbar.ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.crystalrangeseekbar.R;
+
+/**
+ * Created by owais.ali on 6/19/2016.
+ */
+public class RangeSlider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.range_slider);
+    }
+}
